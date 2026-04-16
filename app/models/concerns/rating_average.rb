1e8558f1a9057ee131094146c7955efe74215c5c
Inkeri <inkeri.ahlstrom@helsinki.fi>
2026-04-16 11:34:57 +0300

    Changed average method

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,7 +2,7 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
-    total_ratings = ratings.reduce(0) { |sum, rating| sum + rating.score }
-    (total_ratings / ratings.count).to_f
+    return 0 if ratings.empty?
+    ratings.map { |r| r.score }.sum / ratings.count.to_f
   end
 end
