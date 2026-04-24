cf1639d5c9db797b2d60ca9c771cac6eb9acb444
Inkeri <inkeri.ahlstrom@helsinki.fi>
2026-04-24 18:24:31 +0300

    rest of week 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
 
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def ensure_that_signed_in
+    redirect_to signin_path, notice: 'you should be signed in' if current_user.nil?
+  end
 end
